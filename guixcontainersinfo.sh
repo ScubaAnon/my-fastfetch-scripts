017f3fcce0febf1eb635d467db4be75e02cb8417
ScubaAnon <69989220+ScubaAnon@users.noreply.github.com>
2026-04-22 17:26:47 +0200

    Desperate attempt at making the code more readable.

--- guixcontainersinfo.sh
+++ guixcontainersinfo.sh
@@ -26,7 +26,9 @@ kb_to_human_readable() {
 }
 
 # First object to be inserted
-jq --arg num "$Num_of_containers" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m🖧 C#\u001b[0m: " + $num }] + .[$index:]' "${FastfetchConfDir}template.json" > "${FastfetchConfDir}tmp.json"
+jq --arg num "$Num_of_containers" --argjson index "$Array_index" \
+'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m🖧 C#\u001b[0m: " + $num }] + .[$index:]' \
+"${FastfetchConfDir}template.json" > "${FastfetchConfDir}tmp.json"
 
 # Fetch information for each Guix container, then create and insert objects to config
 # TODO: Should maybe have a check on whether $containers is empty too, but eh.
@@ -39,7 +41,9 @@ for pid in "${containers[@]}"; do
 
     KB_Human=$(kb_to_human_readable "$KB")
 
-    jq --arg PID "$pid" --arg hostname "$hostname" --arg KB "$KB_Human" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $hostname + ": " + $KB + " memory usage - PID " + $PID }] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+    jq --arg PID "$pid" --arg hostname "$hostname" --arg KB "$KB_Human" --argjson index "$Array_index" \
+	'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $hostname + ": " + $KB + " memory usage - PID " + $PID }] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
 done
 
 if [[ -n ${#dockercontainers[@]} ]]; then
@@ -51,7 +55,9 @@ if [[ -n ${#dockercontainers[@]} ]]; then
         (( KB_Total += $(echo "${mem%B}" | numfmt --from=auto --to-unit=1024) ))
         mem=$(echo "$mem" | sed 's/\([0-9.]\)\([A-Z]\)/\1 \2/')
         
-        jq --arg name "$name" --arg mem "$mem" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $name + ": " + $mem + " memory usage" }] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+        jq --arg name "$name" --arg mem "$mem" --argjson index "$Array_index" \
+		'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $name + ": " + $mem + " memory usage" }] + .[$index:]' \
+		"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
     done
 fi
 
@@ -60,18 +66,26 @@ KB_Total=$(kb_to_human_readable "$KB_Total")
 # Finish container section, proceed (else) with VMs sections if VMs are detected
 if [[ -z ${#vms[@]} ]]; then
     ((++Array_index))
-    jq --arg total "$KB_Total" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' "${FastfetchConfDir}tmp.json" > "${FastfetchConfDir}config.jsonc"
+    jq --arg total "$KB_Total" --argjson index "$Array_index" \
+	'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" > "${FastfetchConfDir}config.jsonc"
 else
     ((++Array_index))
-    jq --arg total "$KB_Total" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+    jq --arg total "$KB_Total" --argjson index "$Array_index" \
+	'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
 
     # Simple linebreak
     ((++Array_index))
-    jq --argjson index "$Array_index" '.modules |= .[:$index] + ["break"] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+    jq --argjson index "$Array_index" \
+	'.modules |= .[:$index] + ["break"] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
 
     # Add VMs section
     ((++Array_index))
-    jq --arg num "$Num_of_vms" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m🖧 VM#\u001b[0m: " + $num }] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+    jq --arg num "$Num_of_vms" --argjson index "$Array_index" \
+	'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m🖧 VM#\u001b[0m: " + $num }] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
 
     KB_Total=0
     for vm in "${vms[@]}"; do
@@ -82,13 +96,17 @@ else
         # Transform $KB into a meaningful string
         KB_Human=$(kb_to_human_readable "$KB")
         
-        jq --arg vm "$vm" --arg KB "$KB_Human" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $vm + ": " + $KB + " memory usage" }] + .[$index:]' "${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
+        jq --arg vm "$vm" --arg KB "$KB_Human" --argjson index "$Array_index" \
+		'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ├ \u001b[0m " + $vm + ": " + $KB + " memory usage" }] + .[$index:]' \
+		"${FastfetchConfDir}tmp.json" | sponge "${FastfetchConfDir}tmp.json"
     done
 
     KB_Total=$(kb_to_human_readable "$KB_Total")
 
     ((++Array_index))
-    jq --arg total "$KB_Total" --argjson index "$Array_index" '.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' "${FastfetchConfDir}tmp.json" > "${FastfetchConfDir}config.jsonc"
+    jq --arg total "$KB_Total" --argjson index "$Array_index" \
+	'.modules |= .[:$index] + [{ "type": "custom", "format": "\u001b[1;38;5;63m  ╰┈➤\u001b[0m Total: " + $total + " of memory" }] + .[$index:]' \
+	"${FastfetchConfDir}tmp.json" > "${FastfetchConfDir}config.jsonc"
 fi
 
 rm "${FastfetchConfDir=}tmp.json"
